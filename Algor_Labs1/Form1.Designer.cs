6481aabc89c9658004375c8126535eb3e429e9c5
TumenSan <sandanov.tumen.sib@mail.ru>
2023-10-15 02:16:39 +0700

    Добавьте файлы проекта.

---
+++ Algor_Labs1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Algor_Labs1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество строк и столбцов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(644, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "количество положительных элементов целочисленной матрицы A[M*N], стоящих на перес" +
+    "ечении нечетных строк и столбцов";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "\" \"";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(672, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "найти произв полож элем последов D1, ... , Dn , распол до первого нулевого элем, " +
+    "заменить этой суммой макс элемент массива";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(523, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Случайное формирование данных",
+            "Минимизация числа вычислений",
+            "Максимизация числа вычислений"});
+            this.comboBox1.Location = new System.Drawing.Point(307, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Случайное формирование данных";
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(28, 186);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(747, 296);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 494);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Algor_Labs1/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Algor_Labs1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*Вариант № 3
+
+1. Найти произведение отрицательных элементов главной 
+    диагонали квадратной матрицы и сравнить с суммой элементов первой строки.
+
+2. Найти множество всех слов, которые встречаются в 
+        каждом из двух заданных предложений. */
+
+        void matrix_count()
+        {
+            int N = 10000; // Количество строк и столбцов
+
+            // Создайте матрицу и инициализируйте её случайными числами (для примера)
+            int[,] A = new int[N, N];
+            Random random = new Random();
+
+            switch (comboBox1.Text)
+            {
+                case ("Случайное формирование данных"):
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            A[i, j] = random.Next(int.MinValue, int.MaxValue); // Генерируйте случайные числа от -10 до 10
+                        }
+                    }
+                    break;
+
+                case ("Минимизация числа вычислений"):
+                    /*
+                    создаем матрицу, в которой все элементы равны 0, 
+                    за исключением элементов,
+                    стоящих на пересечении нечетных строк и столбцов. 
+                    Эти элементы могут быть установлены в положительные значения.
+                     */
+                    for (int i = 1; i < N; i += 2) // Нечетные строки
+                    {
+                        for (int j = 1; j < N; j += 2) // Нечетные столбцы
+                        {
+                            A[i, j] = 1; // Минимизируем вычисления, устанавливая значения в 1
+                        }
+                    }
+                    break;
+
+                case ("Максимизация числа вычислений"):
+                    /*
+                     создаем матрицу случайных чисел, 
+                     а затем изменяем каждый элемент на главной 
+                     диагонали на случайное отрицательное число. 
+                     Это гарантирует, что алгоритм будет выполнять 
+                     максимальное количество сравнений отрицательных элементов 
+                     на главной диагонали и сумму элементов первой строки
+                    */
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            A[i, j] = random.Next(-10, 10); // Генерация случайных чисел от -10 до 10
+                        }
+                    }
+
+                    // Заставим элементы на главной диагонали различаться
+                    for (int i = 0; i < N; i++)
+                    {
+                        A[i, i] = random.Next(-10, 0); // Генерация случайных отрицательных чисел
+                    }
+                    break;
+            }
+
+            // Подсчитайте количество положительных элементов
+            int count = 0;
+
+            for (int i = 1; i < N; i += 2) // Нечетные строки
+            {
+                for (int j = 1; j < N; j += 2) // Нечетные столбцы
+                {
+                    if (A[i, j] > 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            label3.Text = count.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            matrix_count();
+        }
+    }
+}
